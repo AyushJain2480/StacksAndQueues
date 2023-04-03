94e2025c5288b4a9662fdba6a52db200c4bd2e87
Ayush Jain <96828089+AyushJain2480@users.noreply.github.com>
2023-04-03 20:37:00 +0530

    Update DuplicatesBrackets.java

--- Stacks/DuplicatesBrackets.java
+++ Stacks/DuplicatesBrackets.java
@@ -36,3 +36,23 @@ class DuplicatesBrackets{
        System.out.println(false);
     }
 }
+
+
+
+
+public static boolean duplicateParanthesis(String expr) {
+      Stack<Character> st = new Stack<>();
+      for(int i = 0; i < expr.length(); i++){
+            char ch = expr.charAt(i);
+            if(ch == ')'){
+          if(st.peek() == '(') return true;
+          while(st.peek() != '('){
+                        st.pop();
+                  }
+                  st.pop();
+            }else{
+                  st.push(ch);
+            }
+      }
+      return false;
+}
